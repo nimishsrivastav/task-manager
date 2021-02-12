3733001acfdf7124c96000371a2148ebbccb73a3
nimish2102 <nimish.srivastav@gmail.com>
2021-02-12 21:45:44 +0530

    Init commit

--- src/emails/account.js
+++ src/emails/account.js
@@ -6,18 +6,44 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const welcomeMail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'TaskManagerAPI<contact.nimishsrivastav@gmail.com>',
-    subject: 'Thanks for using Task Manager API!',
-    text: `Welcome to the API ${name}!\n You can bla bla bla bla bla bla bla.`
+    from: '<email address>',
+    subject: 'Thanks for trying Task Manager API!',
+    html: 
+    `<div style="background-color: #f1f1f1; padding: 40px; font-family: 'Google Sans', Roboto,RobotoDraft,Helvetica,Arial,sans-serif;">
+        <h3>Hello ${name},</h3>
+        <p>Thanks for trying this API</p>
+        <p>This is simple task manager API created using Node.js and MongoDB and hosted on Heroku.</p>
+        <p><i>Features of this API: </i></p>
+        <p>
+          <ui>
+            <li>You can create, read, update, delete users and tasks.</li>
+            <li>Mails will be sent to your email address (if you enter correctly! :P) on creating new user and deleting user.</li>
+            <li>Plus, repo is available on <a href="https://github.com/nimishsrivastav/task-manager">GitHub</a>. So you can make changes and use it as your own!</li>
+          </ul>
+        </p>
+        <br>
+        <h5><strong>Note: </strong>DO NOT reply to this email as this mail address is not monitored. 
+        If you want to connect, please reply to <email address></h5>
+     </div>`
   });
 }
 
 const thanksMail = (email, name) => {
   sgMail.send({
     to: email,
-    from: 'TaskManagerAPI<contact.nimishsrivastav@gmail.com>',
-    subject: 'Thanks for using Task Manager API!',
-    text: `Thanks for using the API ${name}!\n You can bla bla bla bla bla bla bla.`
+    from: '<email address>',
+    replyTo: '<email address>',
+    subject: 'Sad to see you go! :(',
+    html: 
+    `<div style="background-color: #f1f1f1; padding: 40px; font-family: 'Google Sans', Roboto,RobotoDraft,Helvetica,Arial,sans-serif;">
+        <h3>Hello ${name},</h3>
+        <p>It's sad that you're discontinuing from this API. Anyway, thanks for giving this a shot!</p>
+        <p>Kindly share your feedback by replying to this email. </p>
+        <p>Thank you once again! :)</p>
+        <br>
+        <h5><strong>Note: </strong>DO NOT reply to this email as this mail address is not monitored. 
+        If you want to connect, please reply to <email address></h5>
+     </div>`
   });
 }
 
@@ -25,14 +51,3 @@ module.exports = {
   welcomeMail,
   thanksMail
 }
-
-// sgMail.send({
-//   to: 'Nimish Srivastav<nimish.srivastav@gmail.com>',
-//   from: 'TaskManagerAPI<contact.nimishsrivastav@gmail.com>',
-//   subject: 'Subject 1',
-//   text: 'hello'
-// }).then(() => {
-//   console.log('Message Sent!');
-// }).catch((e) => {
-//   console.log(e.response.body);
-// });
